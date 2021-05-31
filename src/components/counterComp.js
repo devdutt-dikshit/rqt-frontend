880ad77ee9b389407c52332b97db50ef7089ae08
Hemant Malik <hemantmalik121@gmail.com>
2021-06-01 01:33:26 +0530

    minor refactoring

--- src/components/counterComp.js
+++ src/components/counterComp.js
@@ -1,26 +1,17 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { increaseCounter, decreaseCounter } from '../redux/counter/counter.actions';
 
-function CounterComp(props) { //this is a sample component for testing if redux is connected properly or not.
+function CounterComp() { //this is a sample component for testing if redux is connected properly or not.
+    const count = useSelector((state)=>state.counter.count);
+    const dispatch = useDispatch();
     return (
         <div>
-            <p>{props.count}</p>
-            <button onClick={props.decreaseCounter}>decrease</button>
-            <button onClick={props.increaseCounter}>increase</button>
+            <p>{count}</p>
+            <button onClick={() => dispatch(decreaseCounter())}>decrease</button>
+            <button onClick={() => dispatch(increaseCounter())}>increase</button>
         </div>
     );
 }
 
-const mapStateToProps = (state) => {
-    return {count: state.counter.count}
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        increaseCounter: () => dispatch(increaseCounter()),
-        decreaseCounter: () => dispatch(decreaseCounter())
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CounterComp);
+export default CounterComp;
